32e8443d5ff7db7a58685f6e326a44a9a9468ead
jdelreal2 <82716714+jdelreal2@users.noreply.github.com>
2021-05-14 10:24:47 -0400

    Create NumberChooserLogicalResponder.c

---
+++ NumberChooserLogicalResponder.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int number;
+printf ("choose a number 1 2 or 3:");
+scanf("%d", &number);
+if (number == 1) {
+    printf ("\nHI");
+}
+if (number == 2) {
+    printf ("\nHELLO");
+}
+if (number == 3) {
+    printf ("\nHALLO");
+}
+
+return 0;
+}
